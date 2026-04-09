7301d821b714fda8d041d7a5d841faf2e250b196
im4m <imamahmed007@gmail.com>
2026-04-09 17:18:32 +0600

    fix: forcefully enable local TV streams in Android via network security config and http scheme

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,7 +8,9 @@ const config: CapacitorConfig = {
     // We removed the remote 'url' so the app loads from the built-in assets
     // This allows the app to communicate with your local video server
     allowNavigation: ['172.28.28.10'],
-    cleartext: true
+    cleartext: true,
+    // Forces the app to load internally via http to match the video streams
+    androidScheme: 'http'
   },
   android: {
     // This setting bypasses the "Mixed Content" security block
